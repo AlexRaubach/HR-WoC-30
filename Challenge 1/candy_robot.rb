2b0b2a331445e89a317a8c11e93278cdaf876720
Antigrapist <antigrapist@gmail.com>
2017-03-13 14:55:07 -0500

    finished challenge and remembered to increment the counter

--- Challenge 1/candy_robot.rb
+++ Challenge 1/candy_robot.rb
@@ -9,12 +9,16 @@ candy_in_bowel = candy_total
 candies_refilled = 0
 
 while i <= time
-	
+	if candy_in_bowel > c[i-1]
+		candy_in_bowel = candy_in_bowel - c[i-1]
+	else
+		candy_in_bowel = 0
+	end
 
 	if i < time && candy_in_bowel < 5 #only refill while the party is ongoing and the bowel is low
 		candies_refilled += candy_total - candy_in_bowel
 		candy_in_bowel = candy_total
 	end
-
+	i += 1
 end
 puts candies_refilled
